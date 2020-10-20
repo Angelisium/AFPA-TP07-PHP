a97330d4fe96743c1092c2f5fd002e37d0c923e3
アレチ <eliard.alexis@gmail.com>
2020-10-20 10:54:18 +0200

    Update index.php

---
+++ EX05/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Calcul impôt</title>
+	</head>
+	<body>
+		<form method="POST">
+			<input type="text" placeholder="Votre nom"><br>
+			<input type="number" placeholder="Votre revenu"><br>
+			<button>OK</button>
+		</form>
+		<!--<?php if($résultat) { ?>-->
+			<p><?=$résultat?></p>
+		<!--<?php } ?>-->
+	</body>
+</html>
