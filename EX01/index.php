c37942bc8a518546cefce83c3261ebcc818de326
アレチ <eliard.alexis@gmail.com>
2020-10-20 08:58:53 +0200

    Update index.php

--- EX01/index.php
+++ EX01/index.php
@@ -7,6 +7,11 @@
 	$ville1 = new Ville();
 	$ville1->setNom("Niort");
 	$ville1->setCode(79000);
+
+	$ville2 = new Ville();
+	$ville2->setNom("Paris");
+	$ville2->setCode(75001);
+
 	var_dump($ville1);
 
 ?>
