91f11d1729e7622f09421c10f8001f1b85b1a546
アレチ <eliard.alexis@gmail.com>
2020-10-20 09:01:36 +0200

    Create index.php

---
+++ EX02/index.php
@@ -0,0 +1,23 @@
+<pre><?php
+
+	spl_autoload_register(function ($name) {
+		require $name . '.class.php';
+	});
+
+	$ville1 = new Ville();
+	$ville1->setNom("Niort");
+	$ville1->setCode(79000);
+
+	$ville2 = new Ville();
+	$ville2->setNom("Paris");
+	$ville2->setCode(75001);
+
+	$ville3 = new Ville();
+	$ville3->setNom("Bordeaux");
+	$ville3->setCode(33000);
+
+	var_dump($ville1);
+	var_dump($ville2);
+	var_dump($ville3);
+
+?></pre>
