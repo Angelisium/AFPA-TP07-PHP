884c40937c06d8b9410fdf135982c376eeb8c3a8
アレチ <eliard.alexis@gmail.com>
2020-10-20 09:57:08 +0200

    Update Form.class.php

--- EX04/Form.class.php
+++ EX04/Form.class.php
@@ -6,6 +6,9 @@
 		public function __construct(array $param) {
 			$this->form .= '<form method="' . ($param['method'] ? $param['method'] : 'GET') . '">';
 			$this->form .= '<fieldset>';
+			if(isset($param['titre'])) {
+				$this->form .= "<legend>" . $param['titre'] . "</legend>";
+			}
 		}
 
 	}
