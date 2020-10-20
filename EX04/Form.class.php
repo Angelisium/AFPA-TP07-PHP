338a156c82afb385b7b8bf92d4de99c7d0650ca1
アレチ <eliard.alexis@gmail.com>
2020-10-20 09:59:35 +0200

    Update Form.class.php

--- EX04/Form.class.php
+++ EX04/Form.class.php
@@ -4,6 +4,10 @@
 		private $form = "";
 
 		public function __construct(array $param) {
+			if(!isset($param['action'])) {
+				throw new Exception('Missing action target');
+			}
+
 			$this->form .= '<form method="' . ($param['method'] ? $param['method'] : 'GET') . '">';
 			$this->form .= '<fieldset>';
 			if(isset($param['titre'])) {
