2a713b2ddb1476ec8bf70f6483172d7725eba9de
アレチ <eliard.alexis@gmail.com>
2020-10-20 10:58:43 +0200

    Update index.php

--- EX05/index.php
+++ EX05/index.php
@@ -1,3 +1,16 @@
+<?php
+	spl_autoload_register(function ($name) {
+		require $name . '.class.php';
+	});
+
+	if($_SERVER['REQUEST_METHOD'] === 'POST'
+		&& isset($_POST['nom'])
+		&& isset($_POST['revenu'])) {
+			$revenu = new Impot($_POST['nom'], $_POST['revenu']);
+			$resultat = $revenu->AfficherImpot();
+	}
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -6,9 +19,10 @@
 		<title>Calcul impôt</title>
 	</head>
 	<body>
+		<pre><?=var_export($_POST, true)?></pre>
 		<form method="POST">
-			<input type="text" placeholder="Votre nom"><br>
-			<input type="number" placeholder="Votre revenu"><br>
+			<input type="text" name="nom" placeholder="Votre nom"><br>
+			<input type="number" name="revenu" placeholder="Votre revenu"><br>
 			<button>OK</button>
 		</form>
 		<!--<?php if($résultat) { ?>-->
